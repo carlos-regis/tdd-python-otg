53a3b62a5cde81b55a1c553e649f2b2ffac909ad
Carlos Regis <cr@carlosregis.com>
2023-09-17 02:42:02 +0000

    Add model for list Items and associated migration

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 
+from lists.models import Item
+
 
 class HomePageTest(TestCase):
     def test_uses_home_template(self) -> None:
@@ -10,3 +12,22 @@ class HomePageTest(TestCase):
         response = self.client.post("/", data={"item_text": "A new list item"})
         self.assertContains(response, "A new list item")
         self.assertTemplateUsed(response, "home.html")
+
+
+class ItemModelTest(TestCase):
+    def test_saving_and_retrieving_items(self) -> None:
+        first_item = Item()
+        first_item.text = "The first (ever) list item"
+        first_item.save()
+
+        first_item = Item()
+        first_item.text = "Item the second"
+        first_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)  # noqa: PT009
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, "The first (ever) list item")  # noqa: PT009
+        self.assertEqual(second_saved_item.text, "Item the second")  # noqa: PT009
